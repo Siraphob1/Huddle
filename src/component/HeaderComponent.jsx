e99ad739574d00a4f3aa1566f2673b8497cccde5
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-15 15:26:12 +0700

    add resposive HeaderComponent

--- src/component/HeaderComponent.jsx
+++ src/component/HeaderComponent.jsx
@@ -6,25 +6,26 @@ import logo from "../images/logo.svg";
 
 const HeaderComponent = () => {
   return (
-    <header className=" relative">
-        <img src={bghero_desktop} alt="bghero_desktop" className="hidden w-full lg:block"/>
-        <img src={bghero_mobile} alt="bghero_mobile" className=" w-full lg:hidden"/>
-        <div    className=" absolute top-0 left-0 w-full h-full 
-                            lg:px-[5rem] lg:py-[3rem]">
-            <section className="flex justify-between">
-                <a href="#"><img src={logo} alt="logo" /></a>
+    <header className=" relative bg-[#ebfbff] ">
+        <img src={bghero_desktop} alt="bghero_desktop" className="hidden w-full xl:block"/>
+        <img src={bghero_mobile} alt="bghero_mobile" className=" w-full xl:hidden"/>
+        <div    className=" absolute top-0 left-0 w-full h-full px-[1rem] py-[2rem] 
+                            lg:px-[4rem] xl:px-[5rem] xl:py-[3rem]">
+            <section className="flex justify-between items-center">
+                <a href="#" className=" w-[100px] h-auto xl:w-auto"><img src={logo} alt="logo" /></a>
                 <button className="btn-try">Try It Free</button>
             </section>
-            <section className="flex
-                                lg:flex-row lg:justify-between lg:mt-[3rem]">
-                <section className=" w-[34%]">
-                    <h1 className="">Build The Community Your Fans Will Love</h1>
-                    <p> Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
+            <section className="flex flex-col mt-[4rem] 
+                                xl:flex-row xl:justify-between xl:items-center xl:mt-[3rem]">
+                <section className="w-[100%] flex flex-col items-center 
+                                    xl:w-[40%] xl:items-start">
+                    <h1 className=" h1-header">Build The Community Your Fans Will Love</h1>
+                    <p className="p-header"> Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
                         Create connections with your users as you engage in genuine discussion. 
                     </p>
-                    <button>Get Started For Free</button>
+                    <button className="btn-getstart">Get Started For Free</button>
                 </section>
-                <section className=" w-[60%]">
+                <section className=" w-[100%] xl:w-[55%]">
                     <img src={mockups} alt="mockups" />
                 </section>
             </section>
