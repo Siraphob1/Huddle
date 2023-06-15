e8742d65ff945e2e12c03362fa57c9da062965f1
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-15 14:40:13 +0700

    add google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        fontPoppins:['Poppins', 'sans-serif'],
+        fontOpen:['Open Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
