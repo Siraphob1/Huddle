2ff954551aaef4a0968ee240a6521db9a44f8e3b
Siraphob1 <Sirapop_t@hotmail.com>
2023-06-16 12:04:30 +0700

    add bottom center card

--- src/component/MainComponent.jsx
+++ src/component/MainComponent.jsx
@@ -27,21 +27,27 @@ const MainComponent = () => {
 
 
   return (
-    <main className=" 
-                      xl:px-[6rem] xl:py-[3rem]">
-      <div className="flex flex-col ">
+    <main className="  py-[9rem] pb-[13rem] relative
+                      xl:px-[6rem] ">
+      <div className="flex flex-col items-center ">
           {data.map((e,i)=>{
-            return <section key={i} className={e.imgfrist ?"flex flex-row-reverse  card-main"
-                                                          :"flex flex-row  card-main"}> 
-                  <section  className="pl-main">
+            return <section key={i} className={e.imgfrist ?"flex flex-col-reverse xl:flex-row-reverse  card-main "
+                                                          :"flex flex-col-reverse xl:flex-row  card-main"}> 
+                  <section  className="padding-main">
                       <h1 className="h1-main">{e.title}</h1>
                       <p className="p-main">{e.paragraph}</p>
                   </section>  
-                  <section className="pl-main">
+                  <section className="padding-main">
                       <img src={e.img} alt={e.img} />
                   </section>  
             </section>
           })}
+      </div>
+      <div className=" absolute bottom-0 left-[50%] -translate-x-1/2 translate-y-1/2 bg-white border rounded-lg py-[3rem] w-[95%] flex flex-col justify-center items-center shadow-lg 
+                        xl:px-[4rem] xl:w-[50%]">
+          <h1 className=" font-fontPoppins font-bold text-[1.2rem] mb-[2rem]
+                          xl:text-[1.8rem]">Ready To Build Your Community?</h1>
+          <button className="btn-getstart">Get Started For Free</button>
       </div>
     </main>
   )
